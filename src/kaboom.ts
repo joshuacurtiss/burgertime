84dbae75ed502e38a725c3eb119ae4be9e8802c3
Josh Curtiss <josh@curtiss.me>
2024-06-07 08:52:22 -0500

    feat: Set Kaboom canvas to be full screen

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -1,12 +1,22 @@
 import kaboom, { KaboomCtx } from 'kaboom';
 
+// Game Constants
+export const GAME_WIDTH = 256;
+export const GAME_HEIGHT = 224;
+
+// Kaboom Instance
 export const k: KaboomCtx = kaboom({
    background: [0, 0, 0],
-   width: 256,
-   height: 224,
-   scale: 3,
+   crisp: true,
+   stretch: true,
+   letterbox: true,
+   scale: 8,
+   width: GAME_WIDTH,
+   height: GAME_HEIGHT,
 });
+k.setFullscreen(true);
 
+// Game Constants (using Kaboom context)
 export const BURGERTIME_BLUE = k.rgb(0, 149, 255);
 
 // Debugging
