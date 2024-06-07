954103321cc44165099b36bb91b95f4a2d38e845
Josh Curtiss <josh@curtiss.me>
2024-06-07 12:00:52 -0500

    feat: Add Chase component to Enemy

--- src/objects/Enemy.ts
+++ src/objects/Enemy.ts
@@ -9,6 +9,10 @@ import {
    Vec2,
    ZComp,
 } from 'kaboom';
+import { canAlive, AliveComp } from '../abilities/Alive';
+import { canChase, ChaseComp } from '../abilities/Chase';
+import { canFreeze, FreezeComp } from '../abilities/Freeze';
+import { canWalk, WalkComp } from '../abilities/Walk';
 
 const {
    add,
@@ -41,7 +45,7 @@ const EnemyCompOptDefaults: EnemyCompOpt = {
    type: 'hotdog',
 };
 
-export function addEnemy(options: Partial<EnemyCompOpt> = {}): GameObj<SpriteComp & AnchorComp & AreaComp & PosComp & ZComp & EnemyComp> {
+export function addEnemy(options: Partial<EnemyCompOpt> = {}): GameObj<SpriteComp & AnchorComp & AreaComp & PosComp & ZComp & EnemyComp & AliveComp & ChaseComp & FreezeComp & WalkComp> {
    const opt = Object.assign({}, EnemyCompOptDefaults, options);
    return add([
       sprite('enemies', { anim: `${opt.type}-walk` }),
@@ -50,6 +54,10 @@ export function addEnemy(options: Partial<EnemyCompOpt> = {}): GameObj<SpriteCom
       area({ shape: new Rect(vec2(0), 7, 15), offset: vec2(0), collisionIgnore: ['powerup'] }),
       z(20),
       opt.type,
+      canAlive(),
+      canChase(),
+      canFreeze(),
+      canWalk(),
       enemy(opt),
    ]);
 }
@@ -68,13 +76,16 @@ export function enemy(options: Partial<EnemyCompOpt> = {}): EnemyComp {
       stun() {
          stunTimer?.cancel();
          stunned = true;
+         this.freeze();
          this.play(`${this.type}-stun`);
          stunTimer = wait(this.stunDelay, () => {
             stunned = false;
+            this.unfreeze();
             this.play(`${this.type}-walk`);
          });
       },
       add() {
+         this.speed = 28;
          this.onCollide('salt', this.stun);
       },
    };
