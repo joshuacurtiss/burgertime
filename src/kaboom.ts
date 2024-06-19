bf87353f5dd24d21f2d6d23e61b14b2993a50370
Josh Curtiss <josh@curtiss.me>
2024-06-19 01:16:32 -0500

    feat: Add getVol util function to Kaboom

--- src/kaboom.ts
+++ src/kaboom.ts
@@ -3,6 +3,7 @@ import kaboom, { KaboomCtx } from 'kaboom';
 // Game Constants
 export const GAME_WIDTH = 256;
 export const GAME_HEIGHT = 224;
+export const DEFAULT_VOL = '50';
 export const DATA_MUSIC_VOL = 'bt_music_vol';
 export const DATA_SFX_VOL = 'bt_sfx_vol';
 
@@ -20,6 +21,11 @@ k.setFullscreen(true);
 
 // Game Constants (using Kaboom context)
 export const BURGERTIME_BLUE = k.rgb(0, 149, 255);
+
+// Sound Handling
+export function getVol(which: string): number {
+   return parseInt(k.getData(which, DEFAULT_VOL)) / 100
+}
 
 // Debugging
 export const urlParams = new URLSearchParams(location.search)
